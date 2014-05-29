63cc559da352be9d489b8e16c1563e9a9f5cf714
Victor Strandmoe <victorstrandmoe@gmail.com>
2014-05-29 23:45:17 +0200

    Sync this shit

--- src/honyJsonParsing/JsonParsing.java
+++ src/honyJsonParsing/JsonParsing.java
@@ -1,4 +1,5 @@
 package honyJsonParsing;
+
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -6,6 +7,8 @@ import java.util.ArrayList;
 
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.JsonToken;
 import com.fasterxml.jackson.core.ObjectCodec;
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -18,17 +21,17 @@ public class JsonParsing {
 	/**
 	 * ArrayList containing the list of urls of the pictures
 	 */
-	private ArrayList<URL> urlList;
+	public static ArrayList<String> urlList;
 	
 	/**
 	 * ArrayList containing the picture information for each picture
 	 */
-	private ArrayList<String> captions;
+	public static ArrayList<String> captions;
 	
 	private String blogName;
 	private URL jsonURL;
 	
-	
+	private int counter = 0;
 	
 	public JsonParsing(URL jsonLocation, String blogNameToFind)
 	{
@@ -39,6 +42,7 @@ public class JsonParsing {
 	public void parseFile() throws JsonParseException, IOException //TODO, simplify and break into smaller methods
 	{
 		
+		String fieldName;
 		//Constructs reader  (Jsonparser)
 		JsonFactory f = new JsonFactory();
 		com.fasterxml.jackson.core.JsonParser jp = f.createJsonParser(jsonURL);
@@ -54,94 +58,147 @@ public class JsonParsing {
 		
 		ObjectNode node = mapper.createObjectNode();
 		
-		int counter1 = 0;
 		//see if this was valid
 		if(current != JsonToken.START_OBJECT)
 		{
 			System.out.println("Error: root should be object!");
 		}
-		
 		//Start searching the json file for the blog information
-		while(jp.nextToken() != JsonToken.END_OBJECT && counter1 <20)
-		{
-			String fieldName = jp.getCurrentName();
-			current = jp.nextToken();
-			
-			//Find the area to start searching the file
-			if(fieldName != null && fieldName.equals("response"))
+		try {
+			while(jp.nextToken() != JsonToken.END_OBJECT)
 			{
-				while(jp.nextToken() != JsonToken.END_OBJECT)
+				
+				fieldName = jp.getCurrentName();
+				current = jp.nextToken();
+				
+				//Find the area to start searching the file
+				if(fieldName != null && fieldName.equals("response"))
 				{
-					current = jp.nextToken();
-					fieldName = jp.getCurrentName();
-					if(fieldName != null && fieldName.equals("posts"))
-					{
-						System.out.println("progress");
-						
-						//repeat this long horrible process 
-						while(jp.nextToken() != JsonToken.END_OBJECT)
+					try {
+						while(jp.nextToken() != JsonToken.END_OBJECT )
 						{
 							current = jp.nextToken();
 							fieldName = jp.getCurrentName();
-							
-							if(fieldName != null && fieldName.equals("caption"))
+							if(fieldName != null && fieldName.equals("posts"))
 							{
-								node = jp.readValueAsTree(); 
 
-								System.out.println(( "Caption: " + node.get(fieldName)));
-								System.out.println("Unprocessed property 1: " + fieldName);
-								System.out.println();
-								System.out.println("------------------------------------------------------------------------");
-								
+								try {
+									//repeat this long horrible process 
+									while(jp.nextToken() != JsonToken.END_OBJECT && counter <=10)
+									{
+										current = jp.nextToken();
+										fieldName = jp.getCurrentName();
+										
+										if(fieldName != null && fieldName.equals("caption"))
+										{
+											//read values as branches (tree)
+											node = jp.readValueAsTree(); 
+											System.out.println(( "caption" + " " + node.get(fieldName)));
+											System.out.println("Unprocessed property 1: " + fieldName);
+											System.out.println();
+											
+											captions.add(node.get(fieldName).asText());
+											
+											do {
+												current = jp.nextToken();
+												fieldName= jp.getCurrentName();
+											}
+											while(fieldName!="image_permalink");
+											node = jp.readValueAsTree();
+											System.out.println(( "image_permalink" + " " + node.get(fieldName)));
+											System.out.println("Unprocessed property 1: " + fieldName);
+											System.out.println();
+											
+											urlList.add(node.get(fieldName).asText());
+											
+											counter++;
+											System.out.println(counter);
+											System.out.println("---------------------------------------------------------");
+											
+										}
+									}
+								}
+								 catch (JsonProcessingException e) {
+									// TODO Auto-generated catch block
+									e.printStackTrace();
+								} catch (IOException e) {
+									// TODO Auto-generated catch block
+									e.printStackTrace();
+								} 
 							}
-							
-							current = jp.nextToken();
-							fieldName = jp.getCurrentName();
-							if(fieldName != null && fieldName.equals("caption"))
-							{
-								node = jp.readValueAsTree(); 
-
-								System.out.println(( "Caption: " + node.get(fieldName)));
-								System.out.println("Unprocessed property 1: " + fieldName);
-								System.out.println();
-								System.out.println("------------------------------------------------------------------------");
+							else 
 								
+							{ 	
+								try {
+							      	jp.skipChildren();
+								}
+							 catch (JsonProcessingException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							} catch (IOException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							} 
 							}
 						}
 					}
-					else 
-						
-					{ 	
-						
-						//the node can now view the  parser as a tree and get values
-						node = jp.readValueAsTree(); 
-						
-						System.out.println(( node.get(fieldName) + " URL"));
-						System.out.println("Unprocessed property 2: " + fieldName);
-				      	jp.skipChildren();
+				 catch (JsonProcessingException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} 
+				} 
+				
+				else
+				{
+					System.out.println("Unprocessed property 2: " + fieldName);
+			      	try {
+			      		//not relevent, skip eveyrthing
+			      		jp.skipChildren();
+					} catch (JsonParseException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
 					}
 				}
-			} 
-			else
-			{
-				System.out.println("Unprocessed property 2: " + fieldName);
-		      	jp.skipChildren();
 			}
-			counter1++;
+			
+		} catch (JsonParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+
+		
 	}
 	
 	
 	public static void main(String args[]) throws MalformedURLException, JsonParseException, IOException
 	{
+		urlList = new ArrayList<String>();
+		captions = new ArrayList<String>();
+		
 		URL url = new URL("http://api.tumblr.com/v2/blog/humansofnewyork.com/posts?api_key=7ag2CJXOuxuW3vlVS5wQG6pYA6a2ZQcSCjzZsAp2pDbVwf3xEk&notes_info=true&filter=text");
 		String blog = "humansofnewyork";
 		
 		JsonParsing parse = new JsonParsing(url, blog);
 		parse.parseFile();
+		
+		System.out.println("--------------------------");
+		System.out.println("URL: ");
+		
+		for(String url_temp : urlList ) {
+			System.out.println(url_temp);
+		}
 	}
-	
 }
+	
 
 
 
